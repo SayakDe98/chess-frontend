f75c4712eb4466caa7f8d1fa9b81f2cbfe696974
SayakDe98 <sayakde22@gmail.com>
2026-03-14 12:50:08 +0530

    Added support for env

--- src/api/websocket.js
+++ src/api/websocket.js
@@ -1,5 +1,8 @@
 export function createSocket(onMessage) {
-  const socket = new WebSocket("ws://localhost:8080/ws");
+  const domain = import.meta.env.VITE_API_DOMAIN;
+
+  const protocol = window.location.protocol === "https:" ? "wss" : "ws";
+  const socket = new WebSocket(`${protocol}://${domain}/ws`);
 
   socket.onmessage = (event) => {
     const data = JSON.parse(event.data);
